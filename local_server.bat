021d4a37083f5938e1da4df6b2107f7bf3400622
misakigr <misakig2mail.ru>
2020-08-07 13:02:49 +0300

    .

---
+++ local_server.bat
@@ -0,0 +1,6 @@
+@echo off
+cd C:\Users\advin\Desktop\GIT_project\misakigr.github.io\site2
+browser-sync start --server --files "*.html"
+::cd C:\Users\advin\Desktop\GIT_project
+::git clone git@github.com:misakigr/misakigr.github.io.git
+pause;
